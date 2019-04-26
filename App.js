ec5484598590b8957441e02625f21e159e3e11f0
Jinsuk Kim <jinsukkim94@gmail.com>
2019-04-26 12:23:12 -0700

    share with desktop

--- App.js
+++ App.js
@@ -8,13 +8,8 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { TouchableOpacity } from 'native-base';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 export default class App extends Component {
   render() {
@@ -22,6 +17,11 @@ export default class App extends Component {
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <TouchableOpacity onPress={() => {}}>
+          <Text>
+            Start
+          </Text>
+        </TouchableOpacity>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
