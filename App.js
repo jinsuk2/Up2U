8c0e519fadb4b8eee9b9a9cd67210d6eaea44aef
Jinsuk Kim <jinsukkim94@gmail.com>
2019-04-29 14:28:02 -0700

    test

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import { StyleSheet, View } from "react-native";
-import Game from "./screens/Game";
+import Game from "./js/screens/Game";
 
-<<<<<<< HEAD
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import { TouchableOpacity } from 'native-base';
@@ -16,31 +15,3 @@ export default class App extends Component {
     );
   }
 }
-=======
-export default () => {
-  return (
-    <View>
-      <Game />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
-});
->>>>>>> 00be1db3fd4d010fdad15cd06f822534993c3b99
