ddacb9595cfff82da85f0bd50d41b64dd531eef9
Jinsuk Kim <jinsukkim94@gmail.com>
2019-04-30 16:58:47 -0700

    Dynamic Photo done

--- js/screens/Home/Home.js
+++ js/screens/Home/Home.js
@@ -4,36 +4,17 @@ import {
     Text,
     Button
 } from 'native-base';
-import NumberInput from 'react-native-numeric-input';
-import { useNavigation, useNavigationParam } from 'react-navigation-hooks';
 import styles from './styles';
-import { withNavigation } from 'react-navigation';
 
-export default Home = ({nav}) => {
-
+export default ({nav}) => {
     return (
         <View>
             <Text>This is the home page.</Text>
-            {/* <NumberInput
-                value={players}
-                type='plus-minus'
-                minValue={2}
-                maxValue={8}
-                initValue={players}
-                onChange={() => 
-                    setPlayers(players + 1)
-                }
-                // containerStyle={styles.container}
-                rounded={true}
-                editable={false}
-                rightButtonBackgroundColor={"#64B5F6"}
-                leftButtonBackgroundColor={"#F8BBD0"}
-            ></NumberInput> */}
             <Button 
                 rounded={true}
-                onPress={() => {
-                    nav.navigate("Name");
-                }}>
+                onPress={() => 
+                    nav.navigate("Name")
+                }>
                 <Text>Start?</Text>
             </Button>
         </View>
