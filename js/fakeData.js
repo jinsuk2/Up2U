e9a8e25e41dae485fd85e30e7556fc06003590f6
Steve Choi <stevechoi93@gmail.com>
2019-04-29 17:47:19 -0700

    code refactor

---
+++ js/fakeData.js
@@ -0,0 +1,10 @@
+export const testUsers = [
+  { name: "1", photo: require("../assets/download-1.jpg"), didWin: true },
+  { name: "2", photo: require("../assets/download-2.jpg"), didWin: true },
+  { name: "3", photo: require("../assets/download-3.jpg"), didWin: true },
+  { name: "4", photo: require("../assets/download-4.jpg"), didWin: true },
+  { name: "5", photo: require("../assets/download.jpg"), didWin: true },
+  { name: "6", photo: require("../assets/images-1.jpg"), didWin: true },
+  { name: "7", photo: require("../assets/images-2.jpg"), didWin: true },
+  { name: "8", photo: require("../assets/images.jpg"), didWin: true }
+];
--- js/screens/Game.js
+++ js/screens/Game.js
@@ -1,53 +1,45 @@
 import React, { useState, useEffect } from "react";
-import { View, TouchableOpacity, Vibration, Text, Image } from "react-native";
+import { View } from "react-native";
 import CountDown from "react-native-countdown-component";
 import Orientation from "react-native-orientation";
-import * as Animatable from "react-native-animatable";
-const PlayerCard = ({ player, color, onPress }) => {
-  return (
-    <TouchableOpacity
-      onPress={() => {
-        onPress();
-        Vibration.vibrate();
-      }}
-      style={{
-        height: "100%",
-        width: "50%",
-        backgroundColor: "white",
-        justifyContent: "center",
-        alignItems: "center"
-      }}
-    >
-      <Animatable.View animation="bounceInDown">
-        <Image
-          style={{ width: 300, height: 300 }}
-          borderRadius={10}
-          source={player.photo}
-        />
-        <Text>{player.name}</Text>
-      </Animatable.View>
-    </TouchableOpacity>
-  );
-};
-const shuffle = array => {
-  return array.sort(() => Math.random() - 0.5);
-};
+import PlayerCard from "../components/PlayerCard";
+import { shuffle } from "../helpers";
+import { testUsers } from "../fakeData";
 export default props => {
   useEffect(() => {
     Orientation.lockToLandscape();
   });
-  const [x, updateState] = useState();
+  const handleOnPressLeft = () => {
+    setKey(key + 1);
+    users[pointer + 1].didWin = false;
+    if (users.length == 2) {
+      props.navigation.navigate("Award", { winner: users[pointer] });
+      setUsers(testUsers);
+    } else if (users.length <= pointer + 3) {
+      setPointer(0);
+      const newUser = users.filter(user => user.didWin);
+      setUsers(shuffle(newUser));
+    } else {
+      setPointer(pointer + 2);
+    }
+  };
+  const handleOnPressRight = () => {
+    setKey(key + 1);
+    users[pointer].didWin = false;
+    if (users.length == 2) {
+      props.navigation.navigate("Award", {
+        winner: users[pointer + 1]
+      });
+      setUsers(testUsers);
+    } else if (users.length <= pointer + 3) {
+      setPointer(0);
+      const newUser = users.filter(user => user.didWin);
 
-  const testUsers = [
-    { name: "1", photo: require("../../assets/download-1.jpg"), didWin: true },
-    { name: "2", photo: require("../../assets/download-2.jpg"), didWin: true },
-    { name: "3", photo: require("../../assets/download-3.jpg"), didWin: true },
-    { name: "4", photo: require("../../assets/download-4.jpg"), didWin: true },
-    { name: "5", photo: require("../../assets/download.jpg"), didWin: true },
-    { name: "6", photo: require("../../assets/images-1.jpg"), didWin: true },
-    { name: "7", photo: require("../../assets/images-2.jpg"), didWin: true },
-    { name: "8", photo: require("../../assets/images.jpg"), didWin: true }
-  ];
+      setUsers(shuffle(newUser));
+    } else {
+      setPointer(pointer + 2);
+    }
+  };
   const [users, setUsers] = useState(testUsers);
   const [pointer, setPointer] = useState(0);
   const [counter, setCounter] = useState(5);
@@ -69,52 +61,20 @@ export default props => {
         digitStyle={{ backgroundColor: "#FFF" }}
         digitTxtStyle={{ color: "black" }}
         timeToShow={["S"]}
-        timeLabels={{ s: "Versus" }}
+        timeLabels={{ s: "" }}
       />
       <PlayerCard
-        key={users[pointer].name}
+        key={users[pointer].name + pointer}
         player={users[pointer]}
         color="red"
-        onPress={() => {
-          setKey(key + 1);
-          users[pointer + 1].didWin = false;
-          if (users.length == 2) {
-            console.log("The winner is", users[pointer].name);
-            props.navigation.navigate("Award", { winner: users[pointer] });
-            setUsers(testUsers);
-          } else if (users.length <= pointer + 3) {
-            setPointer(0);
-            const newUser = users.filter(user => user.didWin);
-
-            setUsers(shuffle(newUser));
-          } else {
-            setPointer(pointer + 2);
-          }
-        }}
+        onPress={handleOnPressLeft}
       />
 
       <PlayerCard
-        key={users[pointer + 1].name}
+        key={users[pointer + 1].name + pointer}
         player={users[pointer + 1]}
         color="blue"
-        onPress={() => {
-          setKey(key + 1);
-          users[pointer].didWin = false;
-          if (users.length == 2) {
-            console.log("The winner is", users[pointer + 1].name);
-            props.navigation.navigate("Award", {
-              winner: users[pointer + 1]
-            });
-            setUsers(testUsers);
-          } else if (users.length <= pointer + 3) {
-            setPointer(0);
-            const newUser = users.filter(user => user.didWin);
-
-            setUsers(shuffle(newUser));
-          } else {
-            setPointer(pointer + 2);
-          }
-        }}
+        onPress={handleOnPressRight}
       />
     </View>
   );
