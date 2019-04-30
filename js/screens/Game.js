4f99c42f826f2f964c9fcddb1eeadd4161280946
Steve Choi <stevechoi93@gmail.com>
2019-04-29 18:53:54 -0700

    object mutability issue fixed with current user

--- js/screens/Game.js
+++ js/screens/Game.js
@@ -1,20 +1,33 @@
 import React, { useState, useEffect } from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import CountDown from "react-native-countdown-component";
 import Orientation from "react-native-orientation";
 import PlayerCard from "../components/PlayerCard";
 import { shuffle } from "../helpers";
 import { testUsers } from "../fakeData";
+import Dialog, { DialogContent } from "react-native-popup-dialog";
+import { sanFranciscoWeights } from "react-native-typography";
 export default props => {
+  const originalUser = JSON.parse(JSON.stringify(testUsers));
+  const [users, setUsers] = useState(testUsers);
+  const [pointer, setPointer] = useState(0);
+  const [key, setKey] = useState(0);
+  const [visible, setVisible] = useState(false);
   useEffect(() => {
     Orientation.lockToLandscape();
   });
   const handleOnPressLeft = () => {
     setKey(key + 1);
+    console.log(users[pointer]);
     users[pointer + 1].didWin = false;
     if (users.length == 2) {
-      props.navigation.navigate("Award", { winner: users[pointer] });
-      setUsers(testUsers);
+      let winner = users[pointer];
+      setUsers(
+        originalUser.map(user => {
+          return { ...user, didWin: true };
+        })
+      );
+      props.navigation.navigate("Award", { winner });
     } else if (users.length <= pointer + 3) {
       setPointer(0);
       const newUser = users.filter(user => user.didWin);
@@ -25,27 +38,40 @@ export default props => {
   };
   const handleOnPressRight = () => {
     setKey(key + 1);
+    console.log(users[pointer + 1]);
     users[pointer].didWin = false;
     if (users.length == 2) {
+      let winner = users[pointer + 1];
+      setUsers(
+        originalUser.map(user => {
+          return { ...user, didWin: true };
+        })
+      );
       props.navigation.navigate("Award", {
-        winner: users[pointer + 1]
+        winner
       });
-      setUsers(testUsers);
+      props.navigation.navigate("Award", { winner });
     } else if (users.length <= pointer + 3) {
       setPointer(0);
       const newUser = users.filter(user => user.didWin);
-
       setUsers(shuffle(newUser));
     } else {
       setPointer(pointer + 2);
     }
   };
-  const [users, setUsers] = useState(testUsers);
-  const [pointer, setPointer] = useState(0);
-  const [counter, setCounter] = useState(5);
-  const [key, setKey] = useState(0);
+
   return (
     <View style={{ flexDirection: "row", height: "100%" }}>
+      <Dialog
+        visible={visible}
+        onTouchOutside={() => {
+          setVisible(false);
+        }}
+      >
+        <DialogContent>
+          <Text>Faster!!</Text>
+        </DialogContent>
+      </Dialog>
       <CountDown
         key={key}
         style={{
@@ -55,26 +81,32 @@ export default props => {
           bottom: "40%",
           zIndex: 1
         }}
-        until={counter}
+        until={5}
         size={30}
-        onFinish={() => {}}
+        onFinish={() => {
+          setVisible(true);
+        }}
         digitStyle={{ backgroundColor: "#FFF" }}
-        digitTxtStyle={{ color: "black" }}
+        digitTxtStyle={{ color: "black", ...sanFranciscoWeights.thin }}
         timeToShow={["S"]}
         timeLabels={{ s: "" }}
       />
       <PlayerCard
-        key={users[pointer].name + pointer}
+        key={"player" + key}
         player={users[pointer]}
         color="red"
-        onPress={handleOnPressLeft}
+        onPress={() => {
+          handleOnPressLeft();
+        }}
       />
 
       <PlayerCard
-        key={users[pointer + 1].name + pointer}
+        key={"player" + key + 1}
         player={users[pointer + 1]}
         color="blue"
-        onPress={handleOnPressRight}
+        onPress={() => {
+          handleOnPressRight();
+        }}
       />
     </View>
   );
