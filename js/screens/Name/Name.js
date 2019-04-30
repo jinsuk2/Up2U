4add21f599f25db3b24149ac03290e1c5c678cb7
Steve Choi <stevechoi93@gmail.com>
2019-04-30 15:02:30 -0700

    resovling merge conflicts

--- js/screens/Name/Name.js
+++ js/screens/Name/Name.js
@@ -1,94 +1,85 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import { Input } from 'react-native-elements';
-import { View } from 'react-native';
-import { Button, Card, CardItem, Body, Text, Content, Container } from 'native-base';
+import React, { useState } from "react";
+import { Input } from "react-native-elements";
+import { View } from "react-native";
+import {
+  Button,
+  Card,
+  CardItem,
+  Body,
+  Text,
+  Content,
+  Container
+} from "native-base";
 
 export default (Name = ({ nav }) => {
-	const [ playerList, setPlayerList ] = useState([]);
-	const [ currentPlayer, setCurr ] = useState('');
+  const [playerList, setPlayerList] = useState([]);
+  const [currentPlayer, setCurr] = useState("");
 
-	//This method should push each text into the position
-	const addPlayer = () => {
-        if (playerList.length >= 8) {
-            alert("You have reached the maximum number of players!");
-        } else if (currentPlayer = ''){
-            alert("You need to enter a nickkity name!");
-        } else if (currentPlayer.length <= 3){
-            alert("4 or more letters plz")
-        } else {
-            setPlayerList(playerList.concat(currentPlayer));
-            setCurr('');
-        }
-    };
-    
-    const addAlert = () => {
-        if (playerList.length >= 8) {
-            return (
-                <Text>Note: You have reached the max. # of players</Text>
-            )
-        }
+  //This method should push each text into the position
+  const addPlayer = () => {
+    if (playerList.length >= 8) {
+      alert("You have reached the maximum number of players!");
+    } else if ((currentPlayer = "")) {
+      alert("You need to enter a nickkity name!");
+    } else if (currentPlayer.length <= 3) {
+      alert("4 or more letters plz");
+    } else {
+      setPlayerList(playerList.concat(currentPlayer));
+      setCurr("");
     }
+  };
 
-	return (
-		<View>
-			{playerList &&
-				playerList.map((data) => {
-					return (
-						<Card>
-							<CardItem>
-									<Text>{data}</Text>
-						 	</CardItem>
-						</Card>
-					);
-				})}
-			<Input
-				value={currentPlayer}
-				placeholder="Enter A Nickname..."
-				onChangeText={(text) => {
-					setCurr(text);
-				}}
-			/>
-			<Button
-				rounded
-				onPress={() => {
-					console.log(playerList);
-					console.log(currentPlayer);
-				}}
-			>
-				<Text>Test Button</Text>
-			</Button>
-			<Button
-				rounded
-				onPress={() => {
-					addPlayer();
-				}}
-			>
-				<Text>Submit</Text>
-			</Button>
-			<Button
-				rounded
-				onPress={() => {
-					nav.navigate('Camera', { playerList: playerList });
-				}}
-			>
-				<Text>Camera</Text>
-			</Button>
-            {
-                addAlert()
-            }
-		</View>
-	);
-});
-=======
-import React from "react";
-import { View, Text } from "react-native";
+  const addAlert = () => {
+    if (playerList.length >= 8) {
+      return <Text>Note: You have reached the max. # of players</Text>;
+    }
+  };
 
-export default Name => {
   return (
     <View>
-      <Text>This is the name page.</Text>
+      {playerList &&
+        playerList.map(data => {
+          return (
+            <Card>
+              <CardItem>
+                <Text>{data}</Text>
+              </CardItem>
+            </Card>
+          );
+        })}
+      <Input
+        value={currentPlayer}
+        placeholder="Enter A Nickname..."
+        onChangeText={text => {
+          setCurr(text);
+        }}
+      />
+      <Button
+        rounded
+        onPress={() => {
+          console.log(playerList);
+          console.log(currentPlayer);
+        }}
+      >
+        <Text>Test Button</Text>
+      </Button>
+      <Button
+        rounded
+        onPress={() => {
+          addPlayer();
+        }}
+      >
+        <Text>Submit</Text>
+      </Button>
+      <Button
+        rounded
+        onPress={() => {
+          nav.navigate("Camera", { playerList: playerList });
+        }}
+      >
+        <Text>Camera</Text>
+      </Button>
+      {addAlert()}
     </View>
   );
-};
->>>>>>> award and result page seperated with reactn
+});
