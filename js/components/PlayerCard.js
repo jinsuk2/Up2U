a13741911c496fc39718515ab4709a83864fccd0
Steve Choi <stevechoi93@gmail.com>
2019-04-30 17:41:35 -0700

    added message for reveal

--- js/components/PlayerCard.js
+++ js/components/PlayerCard.js
@@ -2,15 +2,14 @@ import React, { Component } from "react";
 import { TouchableOpacity, Image, Text, Vibration } from "react-native";
 import * as Animatable from "react-native-animatable";
 export default class PlayerCard extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   handleViewRef = ref => (this.view = ref);
-  bounce = () =>
-    this.view
-      .bounce(800)
-      .then(endState =>
-        console.log(endState.finished ? "bounce finished" : "bounce cancelled")
-      );
+
   render() {
-    const { player, color, onPress } = this.props;
+    const { player, onPress } = this.props;
     return (
       <TouchableOpacity
         key={player.name}
--- js/navigation/RootStackNavigator.js
+++ js/navigation/RootStackNavigator.js
@@ -59,7 +59,7 @@ const AppStack = createStackNavigator(
   },
   {
     headerMode: "none",
-    initialRouteName: "Init"
+    initialRouteName: "Play"
   }
 );
 
